f1d62bd600175a255237fe96fc98e2ba8bf1989c
Diptomoy Das <diptomoydad@gmail.com>
2026-04-06 04:28:35 +0530

    Delete my-project/midnight-starter-template directory

--- my-project/midnight-starter-template/Private Veterinary Record Vault/dist/config.d.ts
+++
@@ -1,8 +0,0 @@
-export declare const currentDir: string;
-export interface Config {
-    readonly logDir: string;
-}
-export declare class LogicTestingConfig implements Config {
-    logDir: string;
-    constructor();
-}
--- my-project/midnight-starter-template/Private Veterinary Record Vault/dist/config.js
+++
@@ -1,7 +0,0 @@
-import path from 'node:path';
-export const currentDir = path.resolve(new URL(import.meta.url).pathname, '..');
-export class LogicTestingConfig {
-    logDir = path.resolve(currentDir, '..', 'logs', 'logic-testing', `${new Date().toISOString()}.log`);
-    constructor() { }
-}
-//# sourceMappingURL=config.js.map
